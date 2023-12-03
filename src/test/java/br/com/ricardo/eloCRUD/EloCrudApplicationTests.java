4f7af7d168d0cee8e51c9388a6c5eaf05aa82a65
RicardooFilho <ricardo.tagami17@gmail.com>
2023-12-03 00:03:06 -0300

    First commit

---
+++ src/test/java/br/com/ricardo/eloCRUD/EloCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricardo.eloCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EloCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
